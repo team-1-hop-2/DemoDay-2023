3befe4c9a83bf5f710e7c75e8002fe96900e5800
Tugurudu <tugurudu@nest-54s-iMac.local>
2023-04-26 18:53:54 +0800

    10-frontend-integrated-quicksanFont

--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Homepage = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold font-quicksand underline">
         Hello world!
       </h1>
     </div>
