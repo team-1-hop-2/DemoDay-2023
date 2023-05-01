8bfd31ff873895b2339dd2589cc0f8f54f72fc79
Flew2k <dulguunpuntsag@gmail.com>
2023-05-01 17:12:54 +0800

    fixed structure and code pretty

--- frontend/src/pages/Sign-in/SignIn.jsx
+++ frontend/src/pages/Sign-in/SignIn.jsx
@@ -1,34 +1,25 @@
-import React from 'react'
-import GoogleIcon from '../../Assets/GoogleIcon'
-
-
+import React from "react";
+import { GoogleIcon } from "../../assets";
 
 const SignIn = () => {
   return (
-    <div className='w-screen bg-gradient-135 from-darkblue  via-greyblue  to-darkpurple  h-screen flex flex-col  items-center '>
+    <div className="flex flex-col items-center w-screen h-screen bg-gradient-135 from-darkblue via-greyblue to-darkpurple ">
+      <div>{/* <Header> */}</div>
 
-        <div>
-            {/* <Header> */}
-        </div>
+      <div className="mt-40 text-2xl font-semibold text-white font-sand p-11">
+        {/* Sign in text */}
+        Sign In
+      </div>
 
-
-        <div className='font-sand font-semibold text-2xl p-11 text-white mt-40'>
-            {/* Sign in text */}
-            Sign In
-        </div>
-
-        <div className='mt-60 flex justify-center color-white '>
-            {/* Sign in button */}
-            <button className='flex flex-wrap justify-around items-center p-1 border rounded border-white w-52 h-13 '>
-                <GoogleIcon/>
-                <text className=' text-white font-sand ml-1'>
-                    Sign in with Google
-                </text>
-            </button>
-        </div>
-
+      <div className="flex justify-center mt-60 color-white ">
+        {/* Sign in button */}
+        <button className="flex flex-wrap items-center justify-around p-1 border border-white rounded w-52 h-13 ">
+          <GoogleIcon />
+          <text className="ml-1 text-white font-sand">Sign in with Google</text>
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
