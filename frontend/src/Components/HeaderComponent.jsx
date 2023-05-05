48e96f30cea6814fa4c157d74efb7e4eeb7675c9
Turbud <turbud@nest-29s-iMac.local>
2023-05-05 18:41:44 +0800

    6-frontend-accounPage

--- frontend/src/Components/HeaderComponent.jsx
+++ frontend/src/Components/HeaderComponent.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useLocation } from "react-router-dom";
-import AccountantPageIcon from "../pages/Assets/accountantPageIcon";
-import HomePageIcon from "../pages/Assets/homePageIcon";
-import StorePageIcon from "../pages/Assets/storePageIcon";
+import { useNavigate } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
+import AccountantPageIcon from "../Assets/accountantPageIcon";
+import HomePageIcon from "../Assets/homePageIcon";
+import StorePageIcon from "../Assets/storePageIcon";
 
-const styles = {
-  underlinedColor: {
-    textDecoration: "none",
-    marginRight: "20px",
-    borderBottom: "2px solid white",
-  },
+// const styles = {
+  // underlinedColor: {
+    // textDecoration: "none",
+    // marginRight: "20px",
+    // borderBottom: "2px solid white",
+  // },
   
-  color: {
-    textDecoration:"none",
-  }
-};
+  // color: {
+    // textDecoration:"none",
+  // }
+// };
 
 const HeaderComponent = () => {
   const navigate = useNavigate();
