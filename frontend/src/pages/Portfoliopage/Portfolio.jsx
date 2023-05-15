3ca3cff7b89e8f3742205c7f7dacd117226cd202
TUUGUS <tuugus@Fulxe-3.local>
2023-05-15 19:05:40 +0800

    json-loader-portf1

--- frontend/src/pages/Portfoliopage/Portfolio.jsx
+++ frontend/src/pages/Portfoliopage/Portfolio.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, {  useState , useEffect} from "react";
 import { PortfHead } from "./comps/Portf-Head";
+import './assets/support.css';
+import MOCK_DATA from "./MOCK_DATA.json"
 export const Portfolio = () => {
+
+
+
+    const [selected, setselected] = useState(null);
+    const [id,setid] = useState(0);
+    useEffect(() => {
+        const fetchData = () => {
+          const selected = MOCK_DATA[id];
+          setselected(selected);
+        };
+    
+        fetchData();
+      }, [id]);
+
+
+      const handleidchange = () => {
+        setid(id+1);
+      };
+
+
+      const handleBclick = () => {
+        handleidchange();
+      }
+
+
     return(
-        <div className="flex justify-center w-screen h-fit">
-            <PortfHead/>
+        <div className="flex justify-center w-screen">
+            <button onClick={handleBclick}>++</button>
+                {selected && <PortfHead data={selected}/>}
         </div>
     )
 }
--- frontend/src/pages/Portfoliopage/comps/Portf-Head.jsx
+++ frontend/src/pages/Portfoliopage/comps/Portf-Head.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
- export const PortfHead = (props) => {
+
+ export function PortfHead ({data}) {
+    console.log(data)
     return (
-        <div className="flex flex-row h-96 w-screen">
-            <div className="flex flex-col h-full w-3/6">
-                <p className="">3453</p>
-                <h1>ret</h1>
-                <p>1314</p>
+        <div className="flex flex-row max-h-39rem w-screen items-center">
+            <div className="flex flex-col h-5/6 w-3/6 justify-center" >
+                
+                <p className="ml-24 font-semibold text-xl "> {data && data.title1}</p>
+                <h1 className="ml-24 font-semibold text-7xl my-10"> {data && data.title2}</h1>
+                <p className="ml-24 font-semibold text-xl"> {data && data.desc1}</p>
             </div>
             <div></div>
         </div>
